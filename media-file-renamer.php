f6e69aa7d84b25f5e2fbfc37b2edf5645993ee9a
TigrouMeow <TigrouMeow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-03 04:14:57 +0000

    Tag 0.4. Support for WPML. Support for Retina plugins such as WP Retina 2x. Adds a '-' between the filename and counter in case of similar files. Mark the media as to be renamed when its name is changed outside the Media Library (avoid all the issues we had before).
    git-svn-id: https://plugins.svn.wordpress.org/media-file-renamer/trunk@566759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- media-file-renamer.php
+++ media-file-renamer.php
@@ -3,11 +3,41 @@
 Plugin Name: Media File Renamer
 Plugin URI: http://www.meow.fr/media-file-renamer
 Description: Renames media files based on their titles and updates the associated posts links.
-Version: 0.34
-Author: Jordy Theiller
+Version: 0.4
+Author: Jordy Meow
 Author URI: http://www.meow.fr
 Remarks: John Godley originaly developed rename-media (http://urbangiraffe.com/plugins/rename-media/), but it wasn't working on Windows, had issues with apostrophes, and was not updating the links in the posts. That's why Media File Renamer exists.
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+
+Originally developed for two of my websites: 
+- Totoro Times (http://www.totorotimes.com) 
+- Haikyo (http://www.haikyo.org)
 */
+
+add_filter( 'attachment_fields_to_save', 'mfrh_attachment_fields_to_save', 1, 2 );
+add_filter( 'media_send_to_editor', 'mfrh_media_send_to_editor', 20, 3 );
+add_action( 'media_row_actions', 'mfrh_media_row_actions', 10, 2 );
+add_action( 'admin_head', 'mfrh_admin_head' );
+
+function mfrh_admin_head() {
+	if ( ! empty( $_GET['mfrh_rename'] ) ) {
+		$mfrh_rename = $_GET['mfrh_rename'];
+		mfrh_attachment_fields_to_save( get_post( $mfrh_rename, ARRAY_A ), null );
+		$_SERVER['REQUEST_URI'] = remove_query_arg(array('mfrh_rename'), $_SERVER['REQUEST_URI']);
+	}
+}
+
+function mfrh_media_row_actions( $actions, $post ) {
+	$require_file_renaming = get_post_meta( $post->ID, '_require_file_renaming', true );
+	if ( $require_file_renaming ) {
+		$newaction['mfrh_rename_file'] = '<a href="?mfrh_rename=' . $post->ID . '" title="Rename files" rel="permalink">' . __( "Rename", 'media-file-renamer' ) . '</a>';
+		return array_merge( $actions, $newaction );
+	}
+	return $actions;
+}
 
 function mfrh_media_send_to_editor($html, $attachment_id, $attachment) {
 	$post =& get_post($attachment_id);
@@ -22,14 +52,33 @@ function mfrh_media_send_to_editor($html, $attachment_id, $attachment) {
 	return $html;
 }
 
+// This is a modified copy of 'wp_unique_filename' in functions.php
+function mfrh_unique_filename( $dir, $filename ) {
+	$number = 2;
+	$info = pathinfo($filename);
+	$ext = !empty($info['extension']) ? '.' . $info['extension'] : '';
+	while ( file_exists( $dir . "/$filename" ) ) {
+		
+		if ( $number == 2 )
+			$filename = str_replace( $ext, "-" . $number++ . $ext, $filename );
+		else {
+			$filename = str_replace( "-" . ($number - 1) . $ext, "-" . $number++ . $ext, $filename );
+		}
+	}
+	return $filename;
+}
+
 function mfrh_attachment_fields_to_save( $post, $attachment ) {
-	
 	// NEW MEDIA FILE INFO (depending on the title of the media)
-	$sanitized_media_title = sanitize_title( $attachment['post_title'] );
+	$sanitized_media_title = sanitize_title( $post['post_title'] );
 	
 	// MEDIA TITLE
-	if ( $post['post_name'] == $sanitized_media_title || empty( $sanitized_media_title ) ) {
-		// Don't do anything if the media title didn't change or if it would turn to an empty string
+	// Get attachment meta data
+	$meta = wp_get_attachment_metadata( $post['ID'] );
+	// Don't do anything if the media title didn't change or if it would turn to an empty string
+	if ( $post['post_name'] == $sanitized_media_title || empty( $sanitized_media_title ) || ( $meta["sanitized_title"] == $sanitized_media_title ) ) {
+		// This media DOES NOT require renaming
+		delete_post_meta( $post['ID'], '_require_file_renaming' );
 		return $post; 
 	}
 	
@@ -42,10 +91,18 @@ function mfrh_attachment_fields_to_save( $post, $attachment ) {
 	
 	// MEDIA LIBRARY USAGE DETECTION
 	// Detects if the user is using the Media Library or 'Add an Image' (while a post edit)
+	// If it is not the Media Library, we don't rename, to avoid issues
 	$media_library_mode = !isset($attachment['image-size']);
+	if ( !$media_library_mode ) {
+		// This media requires renaming
+		if ( !get_post_meta( $post['ID'], '_require_file_renaming' ) )
+			add_post_meta( $post['ID'], '_require_file_renaming', true );
+		return $post;
+	}
 	
-	$new_filename = strtolower( wp_unique_filename( $directory, $sanitized_media_title . '.' . $ext ) );
-	$new_filepath = $directory . '/' . $new_filename ; // '/' should be used EVEN on a Windows based server
+	// LET'S RENAME
+	$new_filename = strtolower( mfrh_unique_filename( $directory, $sanitized_media_title . '.' . $ext ) );
+	$new_filepath = trailingslashit( $directory ) . $new_filename ; // '/' should be used EVEN on a Windows based server
 	// If the new file already exists, it's a weird case, let's do nothing.
 	if ( file_exists( $new_filepath ) === true ) {
 		trigger_error( "Media File Renamer wants to rename a file to " + $new_filepath + " but it already exists.", E_USER_NOTICE );
@@ -64,39 +121,80 @@ function mfrh_attachment_fields_to_save( $post, $attachment ) {
 		add_post_meta( $post['ID'], '_original_filename', $old_filename );
 
 	// Rename the main media file.
-	rename( $old_filepath, $new_filepath );
+	if ( !rename( $old_filepath, $new_filepath ) ) {
+		trigger_error( "Media File Renamer could not find the file" + $old_filepath + ".", E_USER_ERROR );
+		return $post;
+	}
 	
 	// Update the attachment meta
-	$meta = wp_get_attachment_metadata( $post['ID'] );
 	$meta['file'] = str_replace( $noext_old_filename, $noext_new_filename, $meta['file'] );
-	$meta["url"] = str_replace( $noext_old_filename, $noext_new_filename, $attachment['url'] );
-
+	$meta["url"] = str_replace( $noext_old_filename, $noext_new_filename, $meta["url"] );
+	$meta["sanitized_title"] = $sanitized_media_title;
+	
 	// Get the article to which belongs this media
 	$article = "";
-	if ( $media_library_mode && !empty($post['post_parent']) ) {
+	if (!empty($post['post_parent']) ) {
 		$article = get_post( $post['post_parent'] );
 		$article->post_content = str_replace( $old_filename, $new_filename, $article->post_content );
+		
+		// WPML: Modify the translations posts as well [THIS IS A COPY PASTE OF A PREVIOUS BLOCK]
+		if ( function_exists( 'icl_object_id' ) ) {
+			$languages = icl_get_languages( 'skip_missing=0' );
+			foreach ( $languages as $language ) {
+				$id = icl_object_id( $post['post_parent'], 'post', true, $language['language_code'] );
+				if ( !is_null( $id ) ) {
+					$wpml_post = get_post( $id );
+					$wpml_post->post_content = str_replace( $old_filename, $new_filename, $wpml_post->post_content );
+					wp_update_post( $wpml_post );
+				}
+			}
+		}
 	}
 	
 	// Loop through the different sizes in the case of an image, and rename them.
 	// Also change the article links if there are any
 	foreach ( $meta['sizes'] as $size => $meta_size ) {
 		$meta_old_filename = $meta['sizes'][$size]['file'];
-		$meta_old_filepath = $directory . '/' . $meta_old_filename;
+		$meta_old_filepath = trailingslashit( $directory ) . $meta_old_filename;
 		$meta_new_filename = str_replace( $noext_old_filename, $noext_new_filename, $meta_old_filename );
-		$meta_new_filepath = $directory . '/' . $meta_new_filename;
+		$meta_new_filepath = trailingslashit( $directory ) . $meta_new_filename;
 		
 		// ak: Double check files exist before trying to rename.
-		if ( file_exists($meta_old_filepath) && ((!file_exists($meta_new_filepath)) || is_writable($meta_new_filepath)) ) {
-		   rename($meta_old_filepath, $meta_new_filepath);
-		   $meta['sizes'][$size]['file'] = $meta_new_filename;
+		if ( file_exists( $meta_old_filepath ) && ( (!file_exists( $meta_new_filepath ) ) || is_writable( $meta_new_filepath ) ) ) {
+		
+			// WP Retina 2x is detected, let's rename those files as well
+			if ( function_exists( 'wr2x_generate_images' ) ) {
+				$wr2x_old_filepath = str_replace( '.' . $ext, '@2x.' . $ext, $meta_old_filepath );
+				$wr2x_new_filepath = str_replace( '.' . $ext, '@2x.' . $ext, $meta_new_filepath );
+				if ( file_exists( $wr2x_old_filepath ) && ( (!file_exists( $wr2x_new_filepath ) ) || is_writable( $wr2x_new_filepath ) ) ) {
+					rename( $wr2x_old_filepath, $wr2x_new_filepath );
+				}
+			}
+		
+			rename( $meta_old_filepath, $meta_new_filepath );
+			$meta['sizes'][$size]['file'] = $meta_new_filename;
 		}
 		
-		if ( $media_library_mode && !empty( $article ) ) {
+		if ( !empty( $article ) ) {
 			$article->post_content = str_replace( $meta_old_filename, $meta_new_filename, $article->post_content );
+			
+			// WPML: Modify the translations posts as well [THIS IS A COPY PASTE OF A PREVIOUS BLOCK]
+			if ( function_exists( 'icl_object_id' ) ) {
+				$languages = icl_get_languages( 'skip_missing=0' );
+				foreach ( $languages as $language ) {
+					$id = icl_object_id( $post['post_parent'], 'post', true, $language['language_code'] );
+					if ( !is_null( $id ) ) {
+						$wpml_post = get_post( $id );
+						$wpml_post->post_content = str_replace( $meta_old_filename, $meta_new_filename, $wpml_post->post_content );
+						wp_update_post( $wpml_post );
+					}
+				}
+			}
 		}
-		
 	}
+	
+	// This media DOES NOT require renaming
+	delete_post_meta( $post['ID'], '_require_file_renaming' );
 	
 	wp_update_attachment_metadata( $post['ID'], $meta );
 	update_attached_file( $post['ID'], $new_filepath );
@@ -108,7 +206,7 @@ function mfrh_attachment_fields_to_save( $post, $attachment ) {
 	//$post['guid'] = $sanitized_media_title . " [" . $post['ID'] . "]";
 	$post['guid'] = $meta["url"];
 	wp_update_post( $post );
-	if ( $media_library_mode && !empty( $article ) ) {
+	if ( !empty( $article ) ) {
 		wp_update_post( $article );
 	}
 	
@@ -119,6 +217,3 @@ function mfrh_attachment_fields_to_save( $post, $attachment ) {
 	
 	return $post;
 }
-
-add_filter( 'attachment_fields_to_save', 'mfrh_attachment_fields_to_save', 1, 2 );
-add_filter( 'media_send_to_editor', 'mfrh_media_send_to_editor', 20, 3 );
--- readme.txt
+++ readme.txt
@@ -1,22 +1,22 @@
 === Plugin Name ===
 Contributors: TigrouMeow
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JAWE2XWH7ZE5U
-Tags: rename, file, media, management, image, renamer
+Tags: rename, file, media, management, image, renamer, wpml, wp-retina-2x
 Requires at least: 3.0.4
 Tested up to: 3.2.1
-Stable tag: 0.34
+Stable tag: 0.4
 
-This plugins allows you to rename physically the media files by updating their titles. It also updates theirs links in the posts automatically.
+Allows you to rename physically the media files by updating their titles. The related posts will be also updated automatically.
 
 == Description ==
 
-With this plugin, by updating the name of the image / media, you will also :
+Thanks to this plugin, by updating the name of the image / media, it will :
 
-*	rename physically the filename
+*	rename physically the filename nicely
+*	update all the references to that media in the associated posts (WPML is supported)
 *	modify the guid of the media
-*	update all the references to that media in the associated post if there is any (img, src, url...)
 
-It has been tested and works ALSO with Windows and IIS. If the name cannot be renamed, a warning will be write to the error log but there will be no crash.
+To avoid issues, the filename will not be changed if you are changing the media name while editing the post. The media will be "marked" as "to be renamed" in WordPress and you will have a "Rename" link appearing in your Media Manager.
 
 
 == Installation ==
@@ -37,10 +37,16 @@ No questions yet.
 
 == Screenshots ==
 
-1. Just like this. Note that the screenshot is very similar to the one from rename-media (0.1).
+1. Very easy.
 
 
 == Changelog ==
+
+= 0.4 =
+* Support for WPML.
+* Support for Retina plugins such as WP Retina 2x.
+* Adds a '-' between the filename and counter in case of similar files.
+* Mark the media as to be renamed when its name is changed outside the Media Library (avoid all the issues we had before).
 
 = 0.34 =
 * The GUID is now updated using the URL of the images and not the post ID + title (http://wordpress.org/support/topic/plugin-media-file-renamer-incorrect-guid-fix-serious-bug?replies=2#post-2239192).
